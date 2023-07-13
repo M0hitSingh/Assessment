068b74cb936c611a81bee48b9ce9a50e9af29899
golchi <golchigeek@gmail.com>
2023-07-13 09:21:42 +0530

    Added Basic Folder Structure

---
+++ routes/v1/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {login,signup} = require('../../controllers/auth.controller')
+const {authorization} = require("../../middleware/authorization")
+
+/**
+ * Endpoint: /v1/auth
+*/
+
+router.post("/signup",signup);
+router.post("/login",login);
+router.route("/me").get(authorization,getMe)
+
+
+module.exports = router;
+
